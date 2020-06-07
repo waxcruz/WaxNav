9dbc5ab4840a46738400cc059955401b1bcd9864
Bill Weatherwax <waxcruz@yahoo.com>
2020-06-06 18:14:23 -0700

    Update information text

--- WaxNav/WaxNavHelp.swift
+++ WaxNav/WaxNavHelp.swift
@@ -36,13 +36,14 @@ class WaxNavHelpViewController: UIViewController {
         1. After you make your search selections, click on the Go button to see the search results. Select a feature in order to see the bearing from your position to the feature as well as its latitude, longitude, altitude, and map distance (miles).\n
         2. Before starting a search clear the search criteria you don't need. The app displays an error message in the bottom of the iPhone screen when you enter conflicting criteria.\n
         3. You can limit searches by minimum altitude (feet) and maximum map distance (miles)
+        4. Click the Sort button to control the order of search results.
             
         Use cases
         1. You know the name of the feature you seek. Enter one or more words of the name in the search. Word order doesn't matter. For example, to find Loma Prieta Peak you can enter Loma Prieta or Prieta Loma.\n
         2. You want to perform a wildcard search on a feature name. Use the wildcard SQL symbol %. For example to search for Mount Umunhum you can enter %um%.\n
-        3. You want to find a feature using the iPhone's heading. Click on the Heading button and then point your iPhone in the direction of the landscape you want to identify. Next hit the Set button to lock into your heading. The iPhone compass detects the slightest movement so once you line up the iPhone to your point then lock into the heading.\n
+        3. You want to find a feature using the iPhone's compass. Point the top of your iPhone screen in the direction of the landscape you want to identify. Click the Heading Search button to lock into your heading. To unlock the Heading Search and resume the compass display, click the Heading Search button again.\n
         4. You know the exact longitude and latitude of a location. Enter the latitude and longitude in the search field. Use decimal degrees for each.\n
-        5. You know the exact bearing to your location. Enter the degrees in the Set field to override the iPhone heading.
+        5. You know the exact bearing to your location. Rotate the top of your iPhone until the compass displayed matches your desired bearing. Click the Heading Search button to lock into your bearing. To unlock the Heading Search and resume the compass display, click the Heading Search button again.
         6. You want to see all features in a field of view. Select the compass point (e.g., E) in the Facing picker. The app will limit features to those within plus or minus 45 degrees of the compass point.
         7. You want to see all features around you. Clear all search fields. Optionally, set minimum altitude and maximum distance/n
 
